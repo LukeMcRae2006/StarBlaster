129c2acbb27ab06a438c34f694e0c2553c58c3d4
Luke McRae <lukemcrae2006@gmail.com>
2025-11-10 13:55:55 -0500

    Class Work Finished

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,6 +7,15 @@ public class PlayerController : MonoBehaviour
     public InputActionReference moveAction;
     public Vector3 moveVector;
     public float playerSpeed;
+    Vector2 minBounds, maxBounds;
+    [SerializeField] private float leftPadding, rightPadding, upPadding, downPadding;
+
+    void Start()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
 
     void Update()
     {
@@ -15,6 +25,9 @@ public class PlayerController : MonoBehaviour
     void Move()
     {
         moveVector = moveAction.action.ReadValue<Vector2>();
-        transform.position += moveVector * playerSpeed * Time.deltaTime;
+        Vector3 newPos = transform.position + moveVector * playerSpeed * Time.deltaTime;
+        newPos.x = Math.Clamp(newPos.x, minBounds.x + leftPadding, maxBounds.x - rightPadding);
+        newPos.y = Math.Clamp(newPos.y, minBounds.y + downPadding, maxBounds.y - upPadding);
+        transform.position = newPos;
     }
 }
